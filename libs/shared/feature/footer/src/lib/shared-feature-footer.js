a2b3ec964c985d14241244f2ca666e02f637615e
kimthao.huynhthi <kimthao.huynhthi@m.citynow.vn>
2022-03-02 23:07:45 +0700

    fix

--- libs/shared/feature/footer/src/lib/shared-feature-footer.js
+++ libs/shared/feature/footer/src/lib/shared-feature-footer.js
@@ -3,7 +3,7 @@ export const Footer = () => {
   return (
     <div className="footer-wrapper">
       <div className="footer-wrapper-inner">
-        <h3 className="footer-">Demo </h3>
+        <h3 className="footer-">Monorepo-demo </h3>
       </div>
     </div>
   );
